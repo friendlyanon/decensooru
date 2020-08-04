4c0284d94e3071aa20d68df92c0ffc201f10f17a
friendlyanon <friendlyanon@users.noreply.github.com>
2020-08-05 01:20:58 +0200

    Add explicit return type

--- src/util/sleep.ts
+++ src/util/sleep.ts
@@ -1,2 +1,2 @@
-export const sleep = <T = unknown>(millis: number, value?: T) =>
+export const sleep = <T = unknown>(millis: number, value?: T): Promise<T> =>
   new Promise(resolve => setTimeout(resolve, millis, value));
