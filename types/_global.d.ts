f41183e09d2eac0465aa2cd7dbb1d04f80dcd637
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-08-04 20:31:26 +0200

    Forgot to add global types

---
+++ types/_global.d.ts
@@ -0,0 +1,42 @@
+declare const d: Document;
+declare const messageId: string;
+
+declare function $<K extends keyof HTMLElementTagNameMap>(
+  selectors: K,
+  root?: ParentNode,
+): HTMLElementTagNameMap[K] | null;
+declare function $<K extends keyof SVGElementTagNameMap>(
+  selectors: K,
+  root?: ParentNode,
+): SVGElementTagNameMap[K] | null;
+declare function $<E extends Element = Element>(
+  selectors: string,
+  root?: ParentNode,
+): E | null;
+
+declare function $$<K extends keyof HTMLElementTagNameMap>(
+  selectors: K,
+  root?: ParentNode,
+): NodeListOf<HTMLElementTagNameMap[K]>;
+declare function $$<K extends keyof SVGElementTagNameMap>(
+  selectors: K,
+  root?: ParentNode,
+): NodeListOf<SVGElementTagNameMap[K]>;
+declare function $$<E extends Element = Element>(
+  selectors: string,
+  root?: ParentNode,
+): NodeListOf<E>;
+
+declare function on(
+  target: EventTarget,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  options?: boolean | AddEventListenerOptions,
+): void;
+
+declare function off(
+  target: EventTarget,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  options?: boolean | AddEventListenerOptions,
+): void;
